f191ce3bcccfd87a1cf35d11e2f947f41cf3c4b7
shoxxdj <dj.shoxx.dj@gmail.com>
2016-11-08 10:56:47 +0100

    change to openshift

--- server.js
+++ server.js
@@ -301,10 +301,18 @@ app.post('/admin/usertolab',isAuthenticated,isAdmin,function(req,res){
 
 
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+var server = app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 
 // Run Server ! 
-var server= app.listen(3000,function(){
+/*var server= app.listen(3000,function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("Running !");
 });
+*/
